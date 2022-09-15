16e708ff3798f3d684660a523441816023ad6d34
EngenheiraFreitas <alessandra.saldanha2009@gmail.com>
2022-09-15 14:03:28 -0300

    Arrendondei mais ainda os campos de input

--- App.js
+++ App.js
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
-    borderRadius:20,
+    borderRadius:50,
   },
   goButton: {
     width: '50%',
@@ -101,6 +101,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 30,
     fontWeight: 'bold',
+    
   },
   imageIcon: {
     width: 150,
